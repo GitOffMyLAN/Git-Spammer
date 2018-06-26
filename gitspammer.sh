2aecf983bc196360f09793dab11437747031652b
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-26 12:25:26 +1000

    got the varibles working

--- gitspammer.sh
+++ gitspammer.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #amount is the amount you want to commit
-while getopts d:s o
-do case "$o" in
-				a)	$amount="$OPTARG";;
-				[?]) echo "Usage: $0 [-a the amount you want to commit]"
-						 exit 1;;
-esac
-done
-$gitspammer -a "$amount" "$@"
-echo $amount
+#while getopts d:s o
+#do case "$o" in
+#				a)	$amount="$OPTARG";;
+#				u) $username="$OPTARG";;
+#				[?]) echo "Usage: $0 [-a the amount you want to commit]"
+#						 exit 1;;
+#esac
+#done
+#"$0" -a "$amount ""$@"
+amount="$1"
+username="$3"
+password="$4"
+repo="$2"
