7a2a068d14d22b194b6644423c0eeb4eb8e777e6
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-26 12:11:15 +1000

    added options to the shell script

--- gitspammer.sh
+++ gitspammer.sh
@@ -1,2 +1,11 @@
-#!/bin/shell
-
+#!/bin/bash
+#amount is the amount you want to commit
+while getopts d:s o
+do case "$o" in
+				a)	$amount="$OPTARG";;
+				[?]) echo "Usage: $0 [-a the amount you want to commit]"
+						 exit 1;;
+esac
+done
+$gitspammer -a "$amount" "$@"
+echo $amount
