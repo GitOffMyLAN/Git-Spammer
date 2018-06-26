7b6ed47c3675e0ef067bb8de579d1cfb407d4382
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-26 11:55:06 +1000

    Delete gitspammer.rb

--- gitspammer.rb
+++
@@ -1,8 +0,0 @@
-#!/bin/ruby
-# The repo that need to changed
-repo = ;
-# The amount of commits wanted
-amountOfCommits = ;
-# The password and username for the git account. To help automatic commiting
-gitUsername = ;
-gitPassword = ;
