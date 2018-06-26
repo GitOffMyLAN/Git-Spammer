ad2bcc73eaf492a3c47c1775ca39d668e8d2493d
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-06-26 11:47:05 +1000

    set up main varibles

---
+++ gitspammer.rb
@@ -0,0 +1,7 @@
+# The repo that need to changed
+repo = ;
+# The amount of commits wanted
+amountOfCommits = ;
+# The password and username for the git account. To help automatic commiting
+gitUsername = ;
+gitPassword = ;
